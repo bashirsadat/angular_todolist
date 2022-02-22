77d7098bba13582bd7f692b38a1b54196e75f0a9
bashir sadat <sadat.sayedbashir@gmail.com>
2022-02-22 15:28:37 -0500

    forheroku servesr

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/first-angualr-app'));
+app.use(express.static(__dirname + '/src/first-angualr-app'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/first-angualr-app/index.html'));});
+'/src/first-angualr-app/index.html'));});
 app.listen(process.env.PORT || 8080);
